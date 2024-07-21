9193becd2772c22d794511a646e0a2639d9c54e5
realFrankieWilson <frankuwill101@gmail.com>
2024-07-21 23:49:57 +0100

    Modified: app.py auth.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 """Module `app`, a basic flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def message():
     """A flask method that returns a simple dictionary"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """A user function that implements the POST /users route"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Imput validations
+    if not email or not password:
+        messge = "email and password are required"
+        return jsonify({"message": message}), 400
+
+    try:
+        # Attempt to register the user
+        user = AUTH.register_user(email=email, password=password)
+        return jsonify({"email": user.email, "message": "user created"}), 201
+    except ValueError:
+        # Handle the case where user is not created successfully
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-`auth module`
+`auth module`, provides method for user authentication
 """
 
 from db import DB
 from user import User
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -24,8 +25,16 @@ def _hash_password(password: str) -> bytes:
     return hashed_pass
 
 
+def _generate_uuid() -> str:
+    """A function that generates and return a new UUID
+    Returns:
+        str: String representation of a new UUID.
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
-    """Auth class to interact with the atuthentication database"""
+    """implements the data authentication functions"""
 
     def __init__(self):
         self._db = DB()
@@ -49,3 +58,37 @@ class Auth:
             raise ValueError(f"User {email} already exists.")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates user credentials
+        Args:
+            email (str): User email to be validated.
+            password (str): User password to be validated.
+        Returns:
+            True: if credentials are valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a new session for the user and returns the session ID
+        Args:
+            email (str): The user's email.
+        Returns:
+            str: The session ID fo the user.
+        """
+        # try:
+        #     user = self._db.find_user_by(email=email)
+        # except NoResultFound:
+        #     raise ValueError(f"No user found with the email {email}")
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        s_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=s_id)
+
+        return s_id
